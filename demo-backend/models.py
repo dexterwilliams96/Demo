fd727580fc388675f29bc987c035a081ba88aafd
Williams <dexter.williams@capgemini.com>
2022-10-13 20:57:15 +0100

    fixed broken merge

--- demo-backend/models.py
+++ demo-backend/models.py
@@ -14,15 +14,9 @@ class Employee(db.Model):
     def __repr__(self):
         return f"User('{self.id}', '{self.name}', '{self.dob}')"
 
-<<<<<<< HEAD
-
-||||||| 2ed072d
-;;
-=======
->>>>>>> 929cd1e95ea1950b85e2ec3f2bf9547aae57f63b
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), default='no name', server_default=String("pre table migration"), nullable=False)
+    name = db.Column(db.String(30), default='no name', server_default="pre table migration", nullable=False)
     content = db.Column(db.Text, nullable=False, default='Empty Content')
     start_date = db.Column(db.DateTime, nullable=False)
     end_date = db.Column(db.DateTime, nullable=False)
