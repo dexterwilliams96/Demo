d1d50c9acbe064ed0337733a420e0ce939611865
Wes Taylor <wes.taylor@capgemini.com>
2022-10-13 09:34:52 +0100

    backend commit

--- demo-backend/app.py
+++ demo-backend/app.py
@@ -1,7 +1,9 @@
+from flask_cors import CORS
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+CORS(app)
 
 app.config[
     'SQLALCHEMY_DATABASE_URI'] = 'postgresql://fjepnvkdtpbzvn:7ad1b5cecd6a642fc8e4f804d7bf71395d9da76075b5e58cc4644dfc67a5d70a@ec2-99-81-137-11.eu-west-1.compute.amazonaws.com/dbgomtkirp4r14'
