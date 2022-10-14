3b1a3546e91667b19d6bc3a591ff14ad99950185
Wes Taylor <wes.taylor@capgemini.com>
2022-10-14 13:35:27 +0100

    amended Employee email field to be unique

--- demo-backend/models.py
+++ demo-backend/models.py
@@ -6,7 +6,7 @@ class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
     dob = db.Column(db.Date, nullable=False)
-    email = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(60), nullable=False)
     tasks = db.relationship('Task')
     comments = db.relationship('Comment')
