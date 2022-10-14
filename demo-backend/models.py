f7975940a2c6196ec287bb341163f9e98235769f
Wes Taylor <wes.taylor@capgemini.com>
2022-10-14 09:42:33 +0100

    added db migration tracking

--- demo-backend/models.py
+++ demo-backend/models.py
@@ -14,19 +14,14 @@ class Employee(db.Model):
     def __repr__(self):
         return f"User('{self.id}', '{self.name}', '{self.dob}')"
 
-<<<<<<< HEAD
 
-||||||| 2ed072d
-;;
-=======
->>>>>>> 929cd1e95ea1950b85e2ec3f2bf9547aae57f63b
 class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), default='no name', server_default=String("pre table migration"), nullable=False)
+    id = db.Column(db.Integer, primary_key=True) 
     content = db.Column(db.Text, nullable=False, default='Empty Content')
     start_date = db.Column(db.DateTime, nullable=False)
     end_date = db.Column(db.DateTime, nullable=False)
     employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=False)
+    name = db.Column(db.String(30), default='Unnamed', nullable=False)
     comments = db.relationship('Comment')
 
     def __repr__(self):
